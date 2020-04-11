705ff7655338433d5de04c44d4c9e55907a12609
Garrett Howard <hylander.garrett@gmail.com>
2020-04-11 14:52:26 -0600

    yellow is gross

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -9,7 +9,7 @@ function Buttons(props) {
                 <button className="btn blue-grey waves-effect waves-light sort-last" onClick={props.sortLast}>Last</button>
             </div>
             <div className="col s6 right-align">
-                <button className="btn amber accent-4 waves-effect waves-light filter-members" onClick={props.filterMembers}>Members</button>
+                <button className="btn green accent-3 waves-effect waves-light filter-members" onClick={props.filterMembers}>Members</button>
                 <button className="btn red darken-2 waves-effect waves-light filter-members" onClick={props.defaultState}>Reset</button>
             </div>
         </div>
