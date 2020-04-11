4c649049aec523330e6d15a08f4b2d4f193cdf65
Garrett Howard <hylander.garrett@gmail.com>
2020-04-11 12:39:00 -0600

    refactored employee table so that state could be used with employees to make sorting easier

--- src/Components/EmployeeTable/EmployeeTable.js
+++ src/Components/EmployeeTable/EmployeeTable.js
@@ -1,36 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Employee from "../Employee/Employee";
-import employees from "../employees.json"
 
-class EmployeeTable extends Component {
-    render() {
-        return (
-            <div className="white-text">
-                <table className="highlight centered purple darken-3">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>First</th>
-                            <th>Last</th>
-                            <th>Role</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {employees.map(employees => (
-                            <Employee
-                                id={employees.id}
-                                first={employees.first}
-                                last={employees.last}
-                                role={employees.role}
-                                email={employees.email}
-                            />
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+function EmployeeTable(props) {
+
+    return (
+        <tbody>
+            {props.employees.map(employees => (
+                <Employee
+                    id={employees.id}
+                    first={employees.first}
+                    last={employees.last}
+                    role={employees.role}
+                    email={employees.email}
+                />
+            ))}
+        </tbody>
+    )
 }
 
 export default EmployeeTable;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { Component } from 'react';
 import EmployeeTable from "../Components/EmployeeTable/EmployeeTable"
 import Header from "../Components/Header/Header";
 import Wrapper from "../Components/Wrapper";
 import Buttons from "../Components/Buttons/Buttons";
+import employees from "../employees.json"
 
 
-function Home() {
-    return (
-        <Wrapper>
-            <Header />
-            <Buttons />
-            <EmployeeTable />
-        </Wrapper>
-    )
+class Home extends Component {
+    state = {
+        employees: employees
+    }
+
+
+    render() {
+        return (
+            <Wrapper>
+                <Header />
+                <Buttons />
+                <div className="white-text">
+                    <table className="highlight centered purple darken-3">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>First</th>
+                                <th>Last</th>
+                                <th>Role</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <EmployeeTable
+                            employees={this.state.employees}
+                        />
+                    </table>
+                </div>
+            </Wrapper >
+        )
+    }
 }
 
 export default Home;
